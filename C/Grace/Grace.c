41c099a4bacdd4978ef63af2798cd0302e98d9d7
Salem-1 <ph.ahmedsalem@gmail.com>
2026-08-15 09:51:17 +0400

    done and I am statisfied with grace for c inshalla

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,8 +1,24 @@
 #include <fcntl.h>
 #include <stdio.h>
-# define PROGRAM "#include <fcntl.h>%c#include <stdio.h>%c# define PROGRAM %c%s%c%c#define FT(x)int main(){ FILE * fd = fopen(%cGrace_kid.c%c, %cw%c);if (fd == NULL){printf(%cFailed to open the file%c); return(0);}fprintf(fd, PROGRAM, 10, 10, 34, PROGRAM, 34, 10, 34, 34, 34, 34, 34, 34, 10, 34, 34, 10, 10, 10);}%cFT(%chi%c);%c/*%c	This is a comment%c*/"
-#define FT(x)int main(){ FILE * fd = fopen("Grace_kid.c", "w");if (fd == NULL){printf("Failed to open the file"); return(0);}fprintf(fd, PROGRAM, 10, 10, 34, PROGRAM, 34, 10, 34, 34, 34, 34, 34, 34, 10, 34, 34, 10, 10, 10);}
-FT("hi");
+
+#define GRACE_DELIVERY(target_file, payload, ...) \
+    fprintf(target_file, payload __VA_OPT__(,) __VA_ARGS__)
+#define PAYLOAD "#include <fcntl.h>%c#include <stdio.h>%c%c#define GRACE_DELIVERY(target_file, payload, ...) %c%c    fprintf(target_file, payload __VA_OPT__(,) __VA_ARGS__)%c#define PAYLOAD %c%s%c%c# define FT(x)  int main() %c%c	{ %c%c		FILE * target_file = fopen(%cGrace_kid.c%c, %cw%c);%c%c		if (target_file == NULL)%c%c		{%c%c			printf(%cFailed to open the file%c);%c%c			return(0);%c%c		}%c%c		GRACE_DELIVERY(target_file, x, 0xa, 0xa, 0xa, 0x5c, 0xa, 0xa, 0x22, x, 0x22, 0xa, 0x5c, 0xa, 0x5c, 0xa, 0x22, 0x22, 0x22, 0x22, 0x5c, 0xa, 0x5c, 0xa, 0x5c, 0xa, 0x22, 0x22, 0x5c, 0xa, 0x5c, 0xa, 0x5c, 0xa, 0x5c, 0xa, 0xa, 0xa, 0xa, 0xa, 0xa, 0xa, 0xa, 0x5c, 0x5c ,0x22, 0xa);%c%c	}%c%c%cFT(PAYLOAD);%c%c/*%c	Remeber those ascii you will need them%c	%cn 0xa, %c 0x5c, %c 0x22%c*/"
+# define FT(x)  int main() \
+	{ \
+		FILE * target_file = fopen("Grace_kid.c", "w");\
+		if (target_file == NULL)\
+		{\
+			printf("Failed to open the file");\
+			return(0);\
+		}\
+		GRACE_DELIVERY(target_file, x, 0xa, 0xa, 0xa, 0x5c, 0xa, 0xa, 0x22, x, 0x22, 0xa, 0x5c, 0xa, 0x5c, 0xa, 0x22, 0x22, 0x22, 0x22, 0x5c, 0xa, 0x5c, 0xa, 0x5c, 0xa, 0x22, 0x22, 0x5c, 0xa, 0x5c, 0xa, 0x5c, 0xa, 0x5c, 0xa, 0xa, 0xa, 0xa, 0xa, 0xa, 0xa, 0xa, 0x5c, 0x5c ,0x22, 0xa);\
+	}
+
+
+FT(PAYLOAD);
+
 /*
-	This is a comment
+	Remeber those ascii you will need them
+	\n 0xa, \ 0x5c, " 0x22
 */
